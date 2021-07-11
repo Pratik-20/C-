25a4fb55b3ea08897449e4899dcf924475354215
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-07-11 23:33:21 +0530

    Add files via upload

---
+++ Addition.cxx
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	double a,b,c;
+	cout<<"Enter First No.: ";
+	cin>>a;
+	cout<<"Enter Second No.: ";
+	cin>>b;
+	c= a+b;
+	cout<<"Addition of both No.s : "<<c;
+	
+	getch();
+	
+}
