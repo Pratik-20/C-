dd712b88eead3642d2fde5a7a437867b756bab6c
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-07-16 23:20:36 +0530

    Add files via upload

---
+++ Divisiors.cxx
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    int i = 1;
+    cout<<"Enter a Number = ";
+    cin>>a;
+    cout<<"Number is = "<<a;
+  for(b=0;b<=a;b++) {
+     if (a%i==0)
+        cout<<i<<"\n";
+    else
+        cout<<"";
+        i+=1;
+  }
+}
