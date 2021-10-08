ddce08e8d81b46e1faecd2b56c0c8dae73967679
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-10-08 14:23:35 +0530

    Add files via upload

---
+++ Print 1_n Numbers.cxx
@@ -0,0 +1,16 @@
+
+//Print numbers from 1 to n without using a semicolon?
+
+
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int i,n;
+	cin>>n;
+	for(i=1;i<=n;i++)	    
+	      cout<<i<<endl;  //New Line
+	       //cout<<i;          //One Line
+	getch();
+}
