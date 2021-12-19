a26530fc10dbd458130eb802e6e3ccfda545cb0b
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-12-19 23:37:51 +0530

    Add files via upload

---
+++ Even Number.cxx
@@ -0,0 +1,18 @@
+//To check Even/ Odd Number
+
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	cout<<"Enter Number :- ";
+	cin>>n;
+	if(n%2==0)
+	cout<<"Even Number";
+	else
+	cout<<"Odd Number";
+	getch();
+}
+	
+	
