f10c076608f6b3e83e780b32c88287ca1f588e01
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-12-19 23:31:44 +0530

    Add files via upload

---
+++ Leap Year.cxx
@@ -0,0 +1,18 @@
+//To check Leap year or regular year.
+
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	cout<<"Enter Year :- ";
+	cin>>n;
+	if(n%4==0)
+	cout<<"Leap Year";
+	else
+	cout<<"Regular Year";
+	getch();
+}
+	
+	
