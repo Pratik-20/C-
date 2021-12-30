720f9756a346c221daec681aa15a96e408e13650
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-12-30 23:12:20 +0530

    Add files via upload

---
+++ String_Size.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+int main ()
+{
+  std::string str ("This is test string");
+  std::cout << "The size of str is " << str.size() << " bytes.\n";
+  return 0;
+}
