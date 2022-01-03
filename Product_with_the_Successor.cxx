f38048985e81d323da98970c95d1dd7fdb104f50
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2022-01-03 13:40:54 +0530

    Add files via upload

---
+++ Product_with_the_Successor.cxx
@@ -0,0 +1,35 @@
+ /*You are given an integer N and integer array A, as the input where N denotes the length of A. Write a program to find the product of every element with its next consecutive integer on the number line and return the sum of all these products.
+ 
+ Example :-
+ Array = [1, 2, 3, 5]
+ Result = 1*2 +253 +34 + 5*6 = 2+6+12+30 = 50
+ */
+ 
+ 
+ #include<iostream.h>
+ #include<conio.h>
+ int main()
+ {
+ 	int n;
+ 	int sum=0;
+ 	
+ 	cout<<"Enter Array Size = ";
+ 	cin>>n;
+ 	int a[n];          
+     // First array to store integer Numbers
+ 	int b[n];
+ 	// Second array to store Successor
+ 	int c[n];
+ 	// Third array to stote integer Numbers and Successor Multiplication
+ 	cout<<"Enter Array = ";
+ 	
+ 	for (int i =0;i<n;i++){
+ 		cin>>a[i];
+ 		b[i]=a[i]+1;
+ 		c[i]= b[i] * a[i];
+ 	    sum= sum+c[i]; 	      
+ 	}
+ 	
+ 	cout<<sum;	 
+  getch(); 
+ }
