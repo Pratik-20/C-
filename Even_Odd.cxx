928ec5a6ca5bcd65ec557fc09270089d9305f8e8
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2022-01-14 22:21:49 +0530

    Add files via upload

---
+++ Even_Odd.cxx
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+	
+	int a;
+	cout<<"Enter Number = ";
+	cin>>a;
+	
+	if(a%2==0)
+		cout<<"Even Number";
+	else
+		cout<<"Odd Number";
+		
+	getch();
+}
